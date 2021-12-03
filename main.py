bbed5160d4907cf4209c2bd946563f3d58288771
Luz <liz.s.r.pr@gmail.com>
2021-12-02 19:08:47 -0800

    further cleaned up main

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import Modules.Utility as util
-import matplotlib.pyplot as plt
-
 
 #----ALL POPULATION LINE GRAPHS----
-
 #util.plot_all_lines()
-
 
 #----GRAPH POINTS FOR A SET OF DATA AND PERFORM A LINEAR REGRESSION ON IT----
 #util.regressions()
